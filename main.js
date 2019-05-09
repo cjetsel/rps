feaf69129812687808aaa285572e5fc15219c2d6
Chelsey Jetsel <crjetsel@gmail.com>
2019-05-08 20:07:15 -0600

    added spock lizard img

--- main.js
+++ main.js
@@ -8,55 +8,56 @@ function computerTurn() {
   return computerChoice;
 }
 
-function play(choice) {
+function play(choose) {
   computerTurn();
-  console.log(computerChoice);
-  if (choice == "rock") {
-    if (choice == computerChoice) {
+  document.getElementById("computer").innerHTML = `${computerChoice}!`;
+  document.getElementById("player").innerHTML = `${choose}!`;
+  if (choose == "rock") {
+    if (choose == computerChoice) {
       result = "tie";
       console.log(result);
-      declare(result);
+      declare(result, choose);
       return result;
     }
     else computerChoice == "scissors" ? result = "win" : result = "lose";
     console.log(result);
-    declare(result);
+    declare(result, choose);
     return result;
   }
-  else if (choice == "paper") {
-    if (choice == computerChoice) {
+  else if (choose == "paper") {
+    if (choose == computerChoice) {
       result = "tie";
       console.log(result);
-      declare(result);
+      declare(result, choose);
       return result;
     }
     computerChoice == "scissors" ? result = "lose" : result = "win";
     console.log(result);
-    declare(result);
+    declare(result, choose);
     return result;
   }
   else {
-    if (choice == computerChoice) {
+    if (choose == computerChoice) {
       result = "tie";
       console.log(result);
-      declare(result);
+      declare(result, choose);
       return result;
     }
     computerChoice == "paper" ? result = "win" : result = "lose";
     console.log(result);
-    declare(result);
+    declare(result, choose);
     return result;
   }
 }
 
-function declare(result) {
+function declare(result, choose) {
   if (result == "win") {
-    document.getElementById("winner").innerHTML = `<h2> ${choice} beats ${computerChoice}! Winner!</h2>`;
+    document.getElementById("winner").innerHTML = `<h1 class="text-center text-success"> Winner!</h1>`;
   }
   else if (result == "lose") {
-    document.getElementById("winner").innerHTML = `<h2> ${computerChoice} beats ${choice}! Loser!</h2>`
+    document.getElementById("winner").innerHTML = `<h1 class="text-center text-danger"> Loser!</h1>`
   } else {
-    document.getElementById("winner").innerHTML = `<h2> It's a tie! Try again?</h2>`
+    document.getElementById("winner").innerHTML = `<h1 class="text-center text-primary"> It's a tie! Try again?</h1>`
   }
 }
 
